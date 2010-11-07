07d8d286eff24caf6f97d81b0a3c70684c3ede60
unknown <chacheon@.mys.agilent.com>
2010-11-07 17:22:32 +0800

    first checkin

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '51e097ee41d78c56b2623c72ae5b0cd2'
+end
